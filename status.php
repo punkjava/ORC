1559188b5a754ff8188b51ae025a8e8b312faf72
pankaj <techstar2012@outlook.com>
2018-08-26 23:15:43 +0530

    new

---
+++ status.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/main_page.css">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!--link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script-->
+
+</head>
+<body>
+
+
+  <header class="header">
+    <div class="logo">
+    <img src="img/fcritlogo.png" style=""> 
+    
+
+        <div id="address">
+        <p><h1> AGNEL CHARITIES'</h1> </p>
+        <p>  FR. C. RODRIGUES INSTITUTE OF TECHNOLOGY</p>
+        <p style="word-spacing: 2px;"> Agnel Technical Education Complex 
+                        Sector 9-A, Vashi, Navi Mumbai,
+                        Maharashtra, India 
+                        PIN - 400703 </p>
+        </div>
+    </div>
+  </header>
+
+
+
+
+<ul>
+  <li><a  href="home.php">Form</a></li>
+  <li><a  href="history.php">Hisotry</a></li>
+  <li><a class="active" href="status.php">Status</a></li>
+ 
+  <li><a href="#about">Logout</a></li>
+</ul>
+
+<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+  <h2>STATUS HERE</h2>
+</div>
+
+</body>
+</html>
